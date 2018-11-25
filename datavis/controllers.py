ab353814057c7343c4064fbaf76d36a5db94f5a7
rk9109 <rk9109@gmail.com>
2018-11-25 02:10:55 -0500

    Added datasets

--- datavis/controllers.py
+++ datavis/controllers.py
@@ -277,7 +277,8 @@ def data_dashboard():
 # @app.route("/datavis/<string:filename>", methods=['GET'])
 @app.route("/datavis", methods=['POST'])
 def data2vis():
-    filename = request.form['filename']
+    filename = 'joined_census.json'
+    # filename = request.form['filename']
     directory = 'datavis/data/'
     global data
     with open(directory + filename) as f:
@@ -287,8 +288,8 @@ def data2vis():
 
 @app.route("/testdata")
 def testdata():
-    # return jsonify(data)
-    return jsonify(data_utils.load_test_dataset())
+    return jsonify(data)
+    # return jsonify(data_utils.load_test_dataset())
 
 @app.route("/inference", methods=['POST'])
 def inference():
